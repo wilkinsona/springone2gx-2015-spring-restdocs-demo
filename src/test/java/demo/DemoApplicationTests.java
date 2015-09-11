c69478a48a84d7ff6dcbffbb3e1da5e4286a7300
Andy Wilkinson <awilkinson@pivotal.io>
2015-09-11 10:53:39 +0100

    Start using Spring REST Docs in the tests

--- src/test/java/demo/DemoApplicationTests.java
+++ src/test/java/demo/DemoApplicationTests.java
@@ -1,14 +1,18 @@
 package demo;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.restdocs.RestDocumentation;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -24,14 +28,21 @@ public class DemoApplicationTests {
 
 	private MockMvc mockMvc;
 
+	@Rule
+	public RestDocumentation restDocumentation = new RestDocumentation(
+			"build/generated-snippets");
+
 	@Before
 	public void setup() {
-		this.mockMvc = webAppContextSetup(this.context).build();
+		this.mockMvc = webAppContextSetup(this.context)
+				.apply(documentationConfiguration(this.restDocumentation))
+				.build();
 	}
 
 	@Test
 	public void index() throws Exception {
-		this.mockMvc.perform(get("/")).andExpect(status().isOk());
+		this.mockMvc.perform(get("/")).andExpect(status().isOk())
+				.andDo(document("index"));
 	}
 
 }
