d04f02c76ef319ad90e59a26112f7aa76f441a6c
Andy Wilkinson <awilkinson@pivotal.io>
2015-09-11 10:48:34 +0100

    Introduce MockMvc-based test

--- src/test/java/demo/DemoApplicationTests.java
+++ src/test/java/demo/DemoApplicationTests.java
@@ -1,16 +1,37 @@
 package demo;
 
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = DemoApplication.class)
+@WebAppConfiguration
 public class DemoApplicationTests {
 
+	@Autowired
+	private WebApplicationContext context;
+
+	private MockMvc mockMvc;
+
+	@Before
+	public void setup() {
+		this.mockMvc = webAppContextSetup(this.context).build();
+	}
+
 	@Test
-	public void contextLoads() {
+	public void index() throws Exception {
+		this.mockMvc.perform(get("/")).andExpect(status().isOk());
 	}
 
 }
